145202f5a3137238fb0363f363b8e876e8a3a3cb
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2023-10-11 15:07:38 +0200

    interface
    Implementation of the interface and the Place class.

--- PetriNet/src/items/Place.java
+++ PetriNet/src/items/Place.java
@@ -1,5 +1,34 @@
 package items;
 
 public class Place {
+	
+	//private fields
+	
+	//the current number of tokens in the place
+	private int token;
+	
+	public Place(int token) {
+		this.token = token;
+	}
+	
+	public int getToken() {
+		return this.token;
+	}
+	
+	private void setToken(int newToken) {
+		this.token = newToken;
+	}
+	
+	/*adds a specified number of tokens to the place*/
+	
+	public void add(int aTokens) {
+		this.setToken(this.getToken() + aTokens); 
+	}
+	
+	/*removes a specified numbers of tokens to the place*/
+	
+	public void remove(int rTokens) {
+		this.setToken(this.getToken() - rTokens);
+	}
 
 }
