13a9eea176eeb9fa41aa86b91612a60030cdbb29
Arzhig <yann.lerquemain@gmail.com>
2023-10-11 16:22:39 +0200

    Update Test1.java

--- PetriNet/src/tests/Test1.java
+++ PetriNet/src/tests/Test1.java
@@ -1,5 +1,10 @@
 package tests;
+import packnp.*;
+import items.*;
 
 public class Test1 {
 
+	public static void main(String[] args) {
+		PetriNetImplementation net1 = new PetriNetImplementation();
+	}
 }
