a4e2d1e0cebc922cdd83272d7a98bd16c8844ab1
Quentin Rabilloud <quentin.rabilloud@imt-atlantique.net>
2023-10-11 15:42:33 +0200

    step
    Implementation of the step method in Transition

--- PetriNet/src/items/Transition.java
+++ PetriNet/src/items/Transition.java
@@ -1,12 +1,31 @@
 package items;
+import java.util.List;
+import items.edge.EdgeIn;
+import items.edge.EdgeOut;
 
 public class Transition {
+	
+	//private fields
+	
+	private List<EdgeOut> outEdges;
+	private List<EdgeIn> inEdges;
 	
 	public Transition() {
 		
 	}
 	
 	public void step() {
-		
+		boolean isTriggerable = false;
+		for (EdgeOut outEdge : outEdges) {
+			isTriggerable = outEdge.isTriggerable() && isTriggerable;
+		}
+		if (isTriggerable) {
+			for (EdgeOut outEdge : outEdges) {
+				outEdge.trigger();
+			}
+			for (EdgeIn inEdge : inEdges) {
+				inEdge.trigger();
+			}
+		}
 	}
 }
