708a9ca2f6d3eac748d9cb5822c071b51d2eb5e5
Arzhig <yann.lerquemain@gmail.com>
2023-10-20 21:00:10 +0200

    First tests

---
+++ PetriNet/src/tests/Test.java
@@ -0,0 +1,100 @@
+package tests;
+
+import packnp.*;
+import items.*;
+
+public class Test {
+	
+	//SYSTEMATIC TESTS
+	public static void initializationTest() {
+		System.out.println("Testing  initialisation  of a brand new PetriNet");
+		try {
+			// A brand new PetriNet should contain no item
+			PetriNetImplementation net = new PetriNetImplementation();
+			if (net.getPlaces().size()!=0) {
+				System.out.println("Err 1.1 : Non-zero number of places in newly created network");
+				System.exit(1);
+			}
+			if (net.getTransitions().size()!=0) {
+				System.out.println("Err 1.1 : Non-zero number of transitions in newly created network");
+				System.exit(1);
+			}	
+		}
+		catch (Exception e) {
+		    System.out.println("Unexpected Exception : " + e);
+		    e.printStackTrace();
+		}
+	}
+	
+	public static void addPlacesTest() {
+		int nbPlaces = 0;
+		
+		System.out.println("Testing addition of places in a PetriNet");
+		
+		PetriNetImplementation net = new PetriNetImplementation();
+		
+		nbPlaces = net.getPlaces().size();
+		
+		//trying to add an incorrect place
+		try {
+			net.add(new Place(-1));
+			System.out.println("Err 2.1 : Added a place with a negative amount of tokens");
+		}
+		/**
+		 * TODO Add BadEntryException class
+		catch (BadEntryException e) {
+		    if (net.getPlaces().size() != nbPlaces)
+			System.out.println("Err 2.1 : Modified number of places though invalid token value");
+		}
+		*/
+		catch (Exception e) {
+		    System.out.println("Err 2.1 : Exception not handled : " + e);
+		    e.printStackTrace();
+		}
+		
+		nbPlaces = net.getPlaces().size();
+		
+		//trying to add 3 correct places
+		try {
+			net.add(new Place(1));
+			net.add(new Place(2));
+			net.add(new Place(3));
+			if (net.getPlaces().size() != nbPlaces+3) {
+				System.out.println("Err 2.2 : Number of places in network didn't go up by 3 after adding 3 places");
+			}
+		}
+		catch (Exception e) {
+		    System.out.println("Err 2.2 : Exception not handled : " + e);
+		    e.printStackTrace();
+		}
+	}
+	
+	public static void addTransitionsTest() {
+		int nbTransitions = 0;
+		
+		System.out.println("Testing addition of Transitions in a PetriNet");
+		
+		PetriNetImplementation net = new PetriNetImplementation();
+		
+		nbTransitions = net.getTransitions().size();
+		
+		//trying to add 2 Transitions
+		try {
+			net.add(new Transition());
+			net.add(new Transition());
+			if (net.getTransitions().size() != nbTransitions+2) {
+				System.out.println("Err 3.1 : Number of transitions in network didn't go up by 2 after adding 2 transitions");
+			}
+		}
+		catch (Exception e) {
+		    System.out.println("Err 3.1 : Exception not handled : " + e);
+		    e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Test.initializationTest();
+		Test.addPlacesTest();
+		Test.addTransitionsTest();
+	}
+}
--- PetriNet/src/tests/Test1.java
+++ PetriNet/src/tests/Test1.java
@@ -5,34 +5,54 @@ import items.*;
 public class Test1 {
 
 	public static void main(String[] args) {
+		
 		//Simple example : 1 origin place, 1 transition
 		PetriNetImplementation net1 = new PetriNetImplementation();
-		Place p1 = new Place(0);
+		Place p1a = new Place(0);
 		Place p2 = new Place(0);
 		Transition t1 = new Transition();
-		net1.add(p1);
+		net1.add(p1a);
 		net1.add(p2);
 		net1.add(t1);
-		net1.add(p1, t1, 3);
+		net1.add(p1a, t1, 3);
 		net1.add(t1, p2, 2);
-		System.out.println("Tokens in p1 :"+p1.getToken()); // Expected 0
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 0
 		net1.getPlace(0).add(4);		
-		System.out.println("Tokens in p1 :"+p1.getToken()); // Expected 4
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 4
 		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 0
 		net1.step(t1); // Remove 3 tokens in p1, add 2 in p2
-		System.out.println("Tokens in p1 :"+p1.getToken()); // Expected 1
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 1
 		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 2
 		net1.step(t1); // Shouldn't be able to fire this one
-		System.out.println("Tokens in p1 :"+p1.getToken()); // Expected 1
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 1
 		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 2
 		t1.getOutEdges().get(0).setValue(1); //Set the edge value to 1
 		net1.step(t1); 
-		System.out.println("Tokens in p1 :"+p1.getToken()); // Expected 0
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 0
 		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 4
 		net1.getPlace(1).remove(4);
 		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 0
 		
+		//Complexifying the system : 2 place origin, 1 transition
 		
-		
+		Place p1b = new Place(0);
+		net1.add(p1b);
+		net1.add(p1b,t1,2);
+		net1.getPlace(2).add(4);
+		net1.getPlace(0).add(1);
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 1
+		System.out.println("Tokens in p1b :"+p1b.getToken()); // Expected 4
+		t1.getOutEdges().get(0).setValue(2); //Set the p1a-t1 edge value to 2
+		net1.step(t1); // Not supposed to fire
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 1
+		System.out.println("Tokens in p1b :"+p1b.getToken()); // Expected 4
+		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 0
+
+		net1.getPlace(0).add(1); // Making the Edge now triggerable
+		System.out.println(t1.getOutEdges().get(0).isTriggerable()); //Verifying
+		net1.step(t1); // Now supposed to trigger
+		System.out.println("Tokens in p1a :"+p1a.getToken()); // Expected 0
+		System.out.println("Tokens in p1b :"+p1b.getToken()); // Expected 2
+		System.out.println("Tokens in p2 :"+p2.getToken()); // Expected 2
 	}
 }
